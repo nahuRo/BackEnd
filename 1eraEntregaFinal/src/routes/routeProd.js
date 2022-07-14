4523e04aeaae031b99bf09273901bf10c5b52686
aguss rodriguez <agussrg80@gmail.com>
2022-07-14 15:11:00 -0300

    FinalPart2

--- 1eraEntregaFinal/src/routes/routeProd.js
+++ 1eraEntregaFinal/src/routes/routeProd.js
@@ -1,43 +1,55 @@
+// para rutas
 const { Router } = require("express");
-const router = Router();
-let productos = [];
+const route = Router();
 
-router.get("/", (req, res) => {
-	try {
-        res.json(productos);
-        res.json(await productos.getAll());
-    } catch (err) { 
-        res.status(500).json({ error: err });
-    }
-});
+// constructores
+const { GeneratorProd } = require("../utils/ConstructorProd");
 
-router.get("/productos/:id", (req, res) => {
-	const { id } = req.params;
-	const buscado = productos.filter((item) => item.id === Number(id));
-	res.json(buscado.length === 0 ? { error: "producto no encontrado" } : buscado);
-});
+// Objeto
+const produto = new GeneratorProd("ProductosFinal.txt");
 
-router.post("/productos", (req, res) => {
-	const { tittle, price, thumbnail } = req.body;
-	let id = productos.length + 1 || 0;
-	productos.push({ tittle, price, thumbnail, id });
-	res.sendStatus(201).json({ tittle, price, thumbnail, id });
-});
+// administracion
+const { isAdmin, permiso } = require("../utils/Admin&URL");
 
-router.put("/productos/:id", (req, res) => {
+// ---- Rutas para productos ----
+
+// Me permite listar todos los productos disponibles ó un producto por su id (disponible para usuarios y administradores)
+route.get("/:id", async (req, res) => {
 	const id = Number(req.params.id);
-	const { tittle, price, thumbnail } = req.body;
-	const indice = productos.findIndex((item) => item.id === id);
-	productos.splice(indice, 1, { tittle, price, thumbnail, id });
-	res.json({ tittle, price, thumbnail, id });
+	const long = await produto.getAll(); // "entre"  por el return
+	long.length >= id ? res.json(await produto.getById(id)) : res.json(await produto.getAll()); // los 2 devuelven objetos
 });
 
-router.delete("/productos/:id", (req, res) => {
+// Para incorporar productos al listado (disponible para administradores)
+route.post("/", isAdmin(permiso), async (req, res) => {
+	const { tittle, price, thumbnail, descripcion, stock } = req.body;
+	const codeBar = Math.floor(Math.random() * (200000 - 100000 + 1) + 100000);
+	let id = (await produto.getAll()).length + 1 || 0;
+	await produto.save({ tittle, price, thumbnail, descripcion, stock, codeBar, timestamp: new Date(), id });
+	res.redirect("/");
+});
+
+// Actualiza un producto por su id (disponible para administradores)
+route.put("/:id", isAdmin(permiso), async (req, res) => {
 	const id = Number(req.params.id);
-	productos = productos.flatMap((item) => (item.id === id ? [] : item));
-	res.json(productos.length === 0 ? { error: "producto no encontrado" } : productos);
+	const { tittle, price, thumbnail, descripcion, stock } = req.body;
+	const codeBar = Math.floor(Math.random() * (200000 - 100000 + 1) + 100000);
+	res.json(produto.update(id, { tittle, price, thumbnail, descripcion, stock, codeBar, timestamp: new Date() })); // nuevo metodo
 });
 
-module.exports = router;
+// Borra un producto por su id (disponible para administradores)
+route.delete("/:id", isAdmin(permiso), async (req, res) => {
+	const id = Number(req.params.id);
+	res.json(await produto.deleteById(id));
+});
 
+// Atajo URLs no validas
+route.use((req, res) => {
+	res.status(404).json({
+		error: -1,
+		descripcion: req.path,
+		método: "no autorizada",
+	});
+});
 
+module.exports = route;
